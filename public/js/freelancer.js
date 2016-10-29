a8018c0b0868e919f5ec0b7240ab5a487221c99b
hnyGuo <ghgood@sjtu.edu.cn>
2016-10-29 13:35:48 +0800

    front-end edit

--- public/js/freelancer.js
+++ public/js/freelancer.js
@@ -8,7 +8,7 @@
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
         event.preventDefault();
     });
 
@@ -29,16 +29,5 @@
             top: 100
         }
     })
-
-    // Floating label headings for the contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
-    });
 
 })(jQuery); // End of use strict
