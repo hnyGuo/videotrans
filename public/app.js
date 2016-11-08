0010607ff546be44b66c8b3164fd26f0422edd60
hnyGuo <ghgood@sjtu.edu.cn>
2016-11-08 23:27:17 +0800

    save

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	// MODIFY THIS TO THE APPROPRIATE URL IF IT IS NOT BEING RUN LOCALLY
-	var socket = io.connect('http://localhost1');
+	var socket = io.connect('http://localhost');
 
 	var canvas = document.getElementById('rgb-video');
 	var context = canvas.getContext('2d');
